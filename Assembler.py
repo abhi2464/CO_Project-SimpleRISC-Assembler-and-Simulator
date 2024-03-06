e183721e5406de4a007cae3fc5991678035c74e8
aamanprime <aamanprime@gmail.com>
2024-03-06 22:17:51 +0530

    removed [] from opcode

--- Assembler.py
+++ Assembler.py
@@ -1,5 +1,5 @@
 rtype = {
-    'opcode':['0110011'],
+    'opcode':'0110011',
     'funct3':{'add':'000','sub':'000', 'sll':'001', 'slt':'010', 'sltu':'011', 'xor':'100', 'srl':'101','or':'110','and':'111'},
     'funct7':{'add':'0000000','sub':'0100000', 'sll':'0000000', 'slt':'0000000', 'sltu':'0000000', 'xor':'0000000', 'srl':'0000000','or':'0000000','and':'0000000'}
 }
@@ -51,7 +51,7 @@ for x in data:
     s1 = x.split(",")[1].strip()
     s2 = x.split(",")[2].strip()
     if (command in rtype['funct3']):
-        opcode = rtype['opcode'][0]
+        opcode = rtype['opcode']
         destbin = register[dest]['address']
         funct3 = rtype['funct3'][command]
         s1bin = register[s1]['address']
