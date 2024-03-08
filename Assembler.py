d8ff3306c7b604701717701ce762df39c34028c8
aamanprime <aamanprime@gmail.com>
2024-03-08 19:48:29 +0530

    bonus added

--- Assembler.py
+++ Assembler.py
@@ -4,7 +4,7 @@ rtype = {
     'funct3':{'add':'000','sub':'000', 'sll':'001', 'slt':'010', 'sltu':'011', 'xor':'100', 'srl':'101','or':'110','and':'111'},
     'funct7':{'add':'0000000','sub':'0100000', 'sll':'0000000', 'slt':'0000000', 'sltu':'0000000', 'xor':'0000000', 'srl':'0000000','or':'0000000','and':'0000000'}
 }
-
+PC = 0
 jtype={
     'opcode':'1101111'
     
@@ -22,44 +22,44 @@ stype = {
 }
 
 registers = {
-    "x0": {"address": "00000", "value": 0},
-    "x1": {"address": "00001", "value": 0},
-    "x2": {"address": "00010", "value": 0},
-    "x3": {"address": "00011", "value": 0},
-    "x4": {"address": "00100", "value": 0},
-    "x5": {"address": "00101", "value": 0},
-    "x6": {"address": "00110", "value": 0},
-    "x7": {"address": "00111", "value": 0},
-    "x8": {"address": "01000", "value": 0},
-    "x9": {"address": "01001", "value": 0},
-    "x10": {"address": "01010", "value": 0},
-    "x11": {"address": "01011", "value": 0},
-    "x12": {"address": "01100", "value": 0},
-    "x13": {"address": "01101", "value": 0},
-    "x14": {"address": "01110", "value": 0},
-    "x15": {"address": "01111", "value": 0},
-    "x16": {"address": "10000", "value": 0},
-    "x17": {"address": "10001", "value": 0},
-    "x18": {"address": "10010", "value": 0},
-    "x19": {"address": "10011", "value": 0},
-    "x20": {"address": "10100", "value": 0},
-    "x21": {"address": "10101", "value": 0},
-    "x22": {"address": "10110", "value": 0},
-    "x23": {"address": "10111", "value": 0},
-    "x24": {"address": "11000", "value": 0},
-    "x25": {"address": "11001", "value": 0},
-    "x26": {"address": "11010", "value": 0},
-    "x27": {"address": "11011", "value": 0},
-    "x28": {"address": "11100", "value": 0},
-    "x29": {"address": "11101", "value": 0},
-    "x30": {"address": "11110", "value": 0},
-    "x31": {"address": "11111", "value": 0},
+    "x0": {"address": "00000", "value": ""},
+    "x1": {"address": "00001", "value": ""},
+    "x2": {"address": "00010", "value": ""},
+    "x3": {"address": "00011", "value": ""},
+    "x4": {"address": "00100", "value": ""},
+    "x5": {"address": "00101", "value": ""},
+    "x6": {"address": "00110", "value": ""},
+    "x7": {"address": "00111", "value": ""},
+    "x8": {"address": "01000", "value": ""},
+    "x9": {"address": "01001", "value": ""},
+    "x10": {"address": "01010", "value": ""},
+    "x11": {"address": "01011", "value": ""},
+    "x12": {"address": "01100", "value": ""},
+    "x13": {"address": "01101", "value": ""},
+    "x14": {"address": "01110", "value": ""},
+    "x15": {"address": "01111", "value": ""},
+    "x16": {"address": "10000", "value": ""},
+    "x17": {"address": "10001", "value": ""},
+    "x18": {"address": "10010", "value": ""},
+    "x19": {"address": "10011", "value": ""},
+    "x20": {"address": "10100", "value": ""},
+    "x21": {"address": "10101", "value": ""},
+    "x22": {"address": "10110", "value": ""},
+    "x23": {"address": "10111", "value": ""},
+    "x24": {"address": "11000", "value": ""},
+    "x25": {"address": "11001", "value": ""},
+    "x26": {"address": "11010", "value": ""},
+    "x27": {"address": "11011", "value": ""},
+    "x28": {"address": "11100", "value": ""},
+    "x29": {"address": "11101", "value": ""},
+    "x30": {"address": "11110", "value": ""},
+    "x31": {"address": "11111", "value": ""},
 }
 
 
 def getregisters(reg):
     try:
-        return registers[reg]["address"]
+        return registers[reg]
     except:
         exit("Register Not Found")
 
@@ -69,14 +69,16 @@ with open('input.txt', 'r') as file:
 
 with open("output.txt", 'w') as file:
     file.writelines("")
-    
+
 for x in data:
+
     temp=re.split(r"[, ]+",x)
-    command = temp[0]
+    command = temp[0].strip()
     if (command in rtype['funct3']):
-        dest = getregisters(temp[1].strip())
-        s1 = getregisters(temp[2].strip())
-        s2 = getregisters(temp[3].strip())
+        PC += 1
+        dest = getregisters(temp[1].strip())["address"]
+        s1 = getregisters(temp[2].strip())["address"]
+        s2 = getregisters(temp[3].strip())["address"]
         opcode = rtype['opcode']
         funct3 = rtype['funct3'][command]
         funct7 = rtype['funct7'][command]
@@ -85,9 +87,10 @@ for x in data:
             file.writelines(f)
         
     elif command=='jal':
+        PC += 1
         opcode=jtype['opcode']
         bin = lambda x : ''.join(reversed( [str((x >> i) & 1) for i in range(20)] ) )
-        reg=getregisters(temp[1].strip())
+        reg=getregisters(temp[1].strip())["address"]
         imm=bin(int(temp[2]))
         # print(imm[0],imm[len(imm)-10:len(imm)],imm[len(imm)-11],imm[len(imm)-19:len(imm)-11],reg,opcode)
         with open("output.txt", 'a') as file:
@@ -95,15 +98,46 @@ for x in data:
             file.writelines(f)
     
     elif (command in stype['funct3']):
+        PC += 1
         bin = lambda x : ''.join(reversed( [str((x >> i) & 1) for i in range(12)] ) )
         opcode = stype['opcode']
-        dest=getregisters(temp[1].strip())
+        dest=getregisters(temp[1].strip())["address"]
         funct3 =stype['funct3'][command]
         imm=bin(int(temp[2]))
-        s1 = getregisters(temp[3].strip())
+        s1 = getregisters(temp[3].strip())["address"]
         # print(imm[len(imm)-1-11:len(imm)-5],dest,s1,funct3,imm[len(imm)-1-4:len(imm)],opcode)
         with open("output.txt", 'a') as file:
             f = f"{imm[len(imm)-1-11:len(imm)-5]}{dest}{s1}{funct3}{imm[len(imm)-1-4:len(imm)]}{opcode}\n"
             file.writelines(f)
-
+    elif command == "halt":
+        PC += 1
         
+        exit("Halted")
+    elif command == "rvrs":
+        PC += 1
+        rs = getregisters(temp[2].strip())   
+        rd = getregisters(temp[1].strip())
+        rd["value"] = (rs["value"])[::-1]
+        with open("output.txt", 'a') as file:
+            f = f"0000000{rd['address']}000{rs['address']}000000000000\n"
+            file.writelines(f)    
+    elif command == "rst":
+        PC += 1
+        for r in registers:
+            registers[r]["value"] = ""
+
+
+    elif command == "mul":
+        PC += 1
+        dest = getregisters(temp[1].strip())
+        s1 = getregisters(temp[2].strip())
+        s2 = getregisters(temp[3].strip())
+        try:
+            result = bin(int(s1["value"],2) * int(s2["value"],2))[2::]
+            result = result[len(result)-32::]
+            dest["value"] = result
+        except:
+            dest["value"] = "0"*32            
+        with open("output.txt", 'a') as file:
+            f = f"0000000{dest['address']}000{s1['address']}{s2['address']}0000000\n"
+            file.writelines(f)        
